a4483a4abb8071a5ee7f63d77db56fe207e16f39
ebuildSlayer <mohammadibrahim.ibrahim9@gmail.com>
2020-03-28 15:28:35 +0500

    Connecting to heroku psql

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const signin = require('./Controllers/signin');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'faceify'
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
   }
 });
 
